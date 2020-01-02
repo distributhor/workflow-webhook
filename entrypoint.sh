18ec13f20e44533bd3bb336f833fdf9bf6bd8928
Wilhelm Krause <distributhor@fastmail.com>
2020-01-02 16:48:47 +0100

    Fix path to jq

--- entrypoint.sh
+++ entrypoint.sh
@@ -31,7 +31,7 @@ else
 
     DATA_JSON="\"repository\":\"$GITHUB_REPOSITORY\",\"ref\":\"$GITHUB_REF\",\"commit\":\"$GITHUB_SHA\",\"trigger\":\"$GITHUB_EVENT_NAME\",\"workflow\":\"$GITHUB_WORKFLOW\""
     if [ -n "$data" ]; then
-        COMPACT_JSON=$(echo -n "$data" | ./jq -c '')
+        COMPACT_JSON=$(echo -n "$data" | /jq -c '')
         WEBHOOK_DATA="{$DATA_JSON,\"data\":$COMPACT_JSON}"
     else
         WEBHOOK_DATA="{$DATA_JSON}"
