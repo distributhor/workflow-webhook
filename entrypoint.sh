8ff9cf3be4661770939d0e68a1da67427853b9f0
distributhor <distributhor@fastmail.com>
2020-10-04 17:21:31 +0100

    Minor cleanup

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,46 +12,34 @@ if [ -z "$webhook_secret" ]; then
     exit 1
 fi
 
-if [ -n "$webhook_type" ] && [ "$webhook_type" == "form-urlencoded" ]; then
-    CONTENT_TYPE="application/x-www-form-urlencoded"
-else
-    CONTENT_TYPE="application/json"
-fi
-
-if [ "$CONTENT_TYPE" == "application/x-www-form-urlencoded" ]; then
-    
-    FORM_DATA="repository=$GITHUB_REPOSITORY&ref=$GITHUB_REF&head=$GITHUB_HEAD_REF&commit=$GITHUB_SHA&event=$GITHUB_EVENT_NAME&workflow=$GITHUB_WORKFLOW"
-
-    if [ -n "$data" ]; then
-        WEBHOOK_DATA="$FORM_DATA&$data"
-    else
-        WEBHOOK_DATA="$FORM_DATA"
-    fi
-
-else
-
-    DATA_JSON="\"repository\":\"$GITHUB_REPOSITORY\",\"ref\":\"$GITHUB_REF\",\"head\":\"$GITHUB_HEAD_REF\",\"commit\":\"$GITHUB_SHA\",\"event\":\"$GITHUB_EVENT_NAME\",\"workflow\":\"$GITHUB_WORKFLOW\""
-
-    if [ -n "$data" ]; then
-        COMPACT_JSON=$(echo -n "$data" | jq -c '')
-        WEBHOOK_DATA="{$DATA_JSON,\"data\":$COMPACT_JSON}"
-    else
-        WEBHOOK_DATA="{$DATA_JSON}"
-    fi
-
-fi
-
-WEBHOOK_SIGNATURE=$(echo -n "$WEBHOOK_DATA" | openssl sha1 -hmac "$webhook_secret" -binary | xxd -p)
-
 WEBHOOK_ENDPOINT=$webhook_url
 
 if [ -n "$webhook_auth" ]; then
     WEBHOOK_ENDPOINT="-u $webhook_auth $webhook_url"
 fi
 
-# Note:
-#   "curl --trace-ascii /dev/stdout" is an alternative to "curl -v", and includes 
-#   the posted data in the output. However, it can't do so for multipart/form-data 
+if [ -n "$webhook_type" ] && [ "$webhook_type" == "form-urlencoded" ]; then
+    CONTENT_TYPE="application/x-www-form-urlencoded"
+    FORM_DATA="repository=$GITHUB_REPOSITORY&ref=$GITHUB_REF&head=$GITHUB_HEAD_REF&commit=$GITHUB_SHA&event=$GITHUB_EVENT_NAME&workflow=$GITHUB_WORKFLOW"
+    if [ -n "$data" ]; then
+        WEBHOOK_DATA="$FORM_DATA&$data"
+    else
+        WEBHOOK_DATA="$FORM_DATA"
+    fi
+else
+    CONTENT_TYPE="application/json"
+    JSON_DATA="\"repository\":\"$GITHUB_REPOSITORY\",\"ref\":\"$GITHUB_REF\",\"head\":\"$GITHUB_HEAD_REF\",\"commit\":\"$GITHUB_SHA\",\"event\":\"$GITHUB_EVENT_NAME\",\"workflow\":\"$GITHUB_WORKFLOW\""
+    if [ -n "$data" ]; then
+        COMPACT_JSON=$(echo -n "$data" | jq -c '')
+        WEBHOOK_DATA="{$JSON_DATA,\"data\":$COMPACT_JSON}"
+    else
+        WEBHOOK_DATA="{$JSON_DATA}"
+    fi
+fi
+
+WEBHOOK_SIGNATURE=$(echo -n "$WEBHOOK_DATA" | openssl sha1 -hmac "$webhook_secret" -binary | xxd -p)
+
+echo "$CONTENT_TYPE $WEBHOOK_ENDPOINT"
 
 curl -k -v \
     -H "Content-Type: $CONTENT_TYPE" \
