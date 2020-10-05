759e919cb6a05a8f5b4c865581159d36a16871cb
distributhor <distributhor@fastmail.com>
2020-10-05 12:32:31 +0100

    Enable -f flag on curl

--- entrypoint.sh
+++ entrypoint.sh
@@ -70,7 +70,7 @@ fi
 
 echo "Content Type: $CONTENT_TYPE"
 
-curl -k -v \
+curl -k -f -v \
     -H "Content-Type: $CONTENT_TYPE" \
     -H "User-Agent: User-Agent: GitHub-Hookshot/760256b" \
     -H "X-Hub-Signature: sha1=$WEBHOOK_SIGNATURE" \
