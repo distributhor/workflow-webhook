73c2caf7402cc1a4bac7897c45e4d88ad0157f0b
distributhor <distributhor@fastmail.com>
2022-12-28 12:08:53 +0000

    Fix SHA1 generation issue: https://github.com/openssl/openssl/issues/19727

--- entrypoint.sh
+++ entrypoint.sh
@@ -87,7 +87,7 @@ else
 
 fi
 
-WEBHOOK_SIGNATURE=$(echo -n "$WEBHOOK_DATA" | openssl sha1 -hmac "$webhook_secret" -binary | xxd -p)
+WEBHOOK_SIGNATURE=$(echo -n "$WEBHOOK_DATA" | openssl dgst -sha1 -hmac "$webhook_secret" -binary | xxd -p)
 WEBHOOK_SIGNATURE_256=$(echo -n "$WEBHOOK_DATA" | openssl dgst -sha256 -hmac "$webhook_secret" -binary | xxd -p |tr -d '\n')
 WEBHOOK_ENDPOINT=$webhook_url
 
